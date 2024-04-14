e7935a574b5942244f3efa99a3bbb2795359554d
Srikanth <srikanth.shenll@gmail.com>
2024-04-14 15:31:42 +0530

    added path

--- server/server.jsx
+++ server/server.jsx
@@ -10,6 +10,20 @@ import App from "../src/App";
 const PORT = process.env.PORT || 3000;
 const app = express();
 console.log(path.resolve(__dirname, "../", "dist"));
+
+fs.readdir('./.amplify-hosting', (err, files) => {
+  files.forEach(file => {
+    // get the details of the file 
+    let fileDetails = fs.lstatSync(path.resolve(directory, file));
+    // check if the file is directory 
+    if (fileDetails.isDirectory()) {
+      console.log('Directory: ' + file);
+    } else {
+      console.log('File: ' + file);
+    }
+  });
+});
+
 app.use(
   express.static("./.amplify-hosting/static", { maxAge: "30d" })
 );
