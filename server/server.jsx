4369b1ac6db39b77e104b997c9fe856dcdf653d4
Srikanth <srikanth.shenll@gmail.com>
2024-04-14 12:47:54 +0530

    added html

--- server/server.jsx
+++ server/server.jsx
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 console.log(path.resolve(__dirname, "../", "dist"));
 app.use(
-  express.static(path.resolve(__dirname, "../../static"), { maxAge: "30d" })
+  express.static("./.amplify-hosting/static", { maxAge: "30d" })
 );
 
 
